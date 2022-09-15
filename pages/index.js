8648ca419757e33e0a8884231b898c9b614fc53c
Develper-Gitonga <aizensalim52@gmail.com>
2022-09-15 21:56:51 +0300

    ch;boilerplate start

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+    {
+        id: "m1",
+        title: "A First Meetup",
+        image: "https://unsplash.com/photos/-SFhuMwFClk",
+        address: "Athens, Greece",
+        description: "This is a First meetup!",
+    },
+
+    {
+        id: "m2",
+        title: "A First Meetup",
+        image: "https://unsplash.com/photos/RwHv7LgeC7s",
+        address: "Chania, Greece",
+        description: "This is a Second meetup!",
+    },
+];
+
+function HomePage() {
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage;
