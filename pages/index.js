1793d0716e60931338e030aeedff63df27525522
Develper-Gitonga <aizensalim52@gmail.com>
2022-09-16 09:49:01 +0300

    ch;boilerplate start

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,45 @@
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://en.wikipedia.org/wiki/John_Wick_(film)#/media/File:John_Wick_TeaserPoster.jpg",
-    address: "Athens, Greece",
-    description: "This is a First meetup!",
-  },
+    {
+        id: "m1",
+        title: "A First Meetup",
+        image:
+        "https://en.wikipedia.org/wiki/John_Wick_(film)#/media/File:John_Wick_TeaserPoster.jpg",
+        address: "Athens, Greece",
+        description: "This is a First meetup!",
+    },
 
-  {
-    id: "m2",
-    title: "A First Meetup",
-    image: "https://unsplash.com/photos/RwHv7LgeC7s",
-    address: "Chania, Greece",
-    description: "This is a Second meetup!",
-  },
+    {
+        id: "m2",
+        title: "A First Meetup",
+        image: "https://unsplash.com/photos/RwHv7LgeC7s",
+        address: "Chania, Greece",
+        description: "This is a Second meetup!",
+    },
 ];
 
-function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />
+function HomePage(props) {
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+        // send http request and fetch data
+        setLoadedMeetups(DUMMY_MEETUPS)
+        
+    }, []);
+
+    return <MeetupList meetups={props.meetups} />
+}
+
+export async function getStaticProps() {
+    //fetch data from an API
+    return {
+        props: {
+            meetups:DUMMY_MEETUPS
+        }, 
+        revalidate: 10
+    };
 }
 
 export default HomePage;
