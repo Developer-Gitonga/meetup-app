1c48e1e63deb0a32e18d4cd723d2cd61b27c0005
Develper-Gitonga <aizensalim52@gmail.com>
2022-09-16 09:02:16 +0300

    ch;boilerplate start

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,16 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+    return (
+
+        <MeetupDetail
+            image='https://en.wikipedia.org/wiki/John_Wick_(film)#/media/File:John_Wick_TeaserPoster.jpg'
+            title='A first meetup'
+            address='Athens, Greece'
+            description='A Nation within the city'
+        />
+        
+    );
+}
+
+export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-    {
-        id: "m1",
-        title: "A First Meetup",
-        image: "https://unsplash.com/photos/-SFhuMwFClk",
-        address: "Athens, Greece",
-        description: "This is a First meetup!",
-    },
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://en.wikipedia.org/wiki/John_Wick_(film)#/media/File:John_Wick_TeaserPoster.jpg",
+    address: "Athens, Greece",
+    description: "This is a First meetup!",
+  },
 
-    {
-        id: "m2",
-        title: "A First Meetup",
-        image: "https://unsplash.com/photos/RwHv7LgeC7s",
-        address: "Chania, Greece",
-        description: "This is a Second meetup!",
-    },
+  {
+    id: "m2",
+    title: "A First Meetup",
+    image: "https://unsplash.com/photos/RwHv7LgeC7s",
+    address: "Chania, Greece",
+    description: "This is a Second meetup!",
+  },
 ];
 
 function HomePage() {
